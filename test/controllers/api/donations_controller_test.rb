e0aa886023a437de01897f079bea1b3f41954763
angelsystem <angelsystem1387@gmail.com>
2015-11-21 23:48:46 -0500

    test

---
+++ test/controllers/api/donations_controller_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class Api::DonationsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+  test "image upload" do
+    test_image = Rails.root.join("test/fixtures/files/test.jpeg")
+    file = Rack::Test::UploadedFile.new(test_image, "image/jpeg")
+    post "/create", :donation => { :picture => file, title: 'mi title', description: "mi description" }
+  end
+end
